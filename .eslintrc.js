50afecb82c61d34a7f0b54283306be75ee4f7c3f
BccSafe <bccsafe5988@gmail.com>
2017-02-26 23:56:07 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "extends": ['eslint-config-airbnb'],
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        }
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+       
+    }
+};

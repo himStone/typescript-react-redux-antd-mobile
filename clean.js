06ee86ff8f578f6bc2603ee6ca86fcb1168c6297
BccSafe <bccsafe5988@gmail.com>
2017-04-18 00:08:19 +0800

    improve clean

--- clean.js
+++ clean.js
@@ -1,66 +1,8 @@
-var glob = require("glob");
-var fs = require("fs");
-var async = require('async');
+var rimraf = require('rimraf');
+var mode = process.argv[2];
+var path = process.cwd() + "/static/" + (mode ? mode : "");
 
-var _mode = process.argv[2]; // clean-dist, clean-cache
-var _path = "";
-
-var myRemove = function(location, next) {
-    fs.readdir(location, function (err, files) {
-        if (files) {
-            async.each(files, function (file, cb) {
-                file = location + '/' + file
-                fs.stat(file, function (err, stat) {
-                    if (err) {
-                        return cb(err);
-                    }
-                    
-                    if (stat.isDirectory()) {
-                        removeFolder(file, cb);
-                    } else {                        
-                        fs.unlink(file, function (err) {
-                            if (err) {
-                                return cb(err);
-                            }
-                            return cb();
-                        })
-                    }
-                })
-            }, function (err) {
-                if (err) return next(err)
-                fs.rmdir(location, function (err) {
-                    return next(err)
-                })
-            })
-        } else {
-            fs.unlink(location, function (err) {
-            })
-        }
-    })
-}
-
-
-if (_mode == "-dist") {
-    _path = "dist/*";
-} else if (_mode == "-dev-cache") {
-    _path = "cache/*.json.gz";
-} else if (_mode == "-dev-bundle") {
-    _path = "cache/*.js";
-} else if (_mode == "-dev-all") {
-    _path = "cache/*";
-}
-
-if (_mode != "") {
-    console.log("clean dir " + process.cwd() + '/static/' + _path);
-    glob(process.cwd() + '/static/' + _path,function(err,files){
-        if (err) throw err;
-        // console.log(files);
-        // Delete files
-        files.forEach(function(item,index,array){
-            myRemove(item, function() {
-                
-            });
-
-        });
-    });
-}
+console.log("start clean dir " + path);
+rimraf(path, function () {
+  console.log("clean dir finish!");
+});
